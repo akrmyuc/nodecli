515c2441c502e7a0989d93f3fd1caedfad1e39ca
akrmyuc <87920616+akrmyuc@users.noreply.github.com>
2022-04-10 18:07:45 +0900

    MarkdownをHTMLに変換した

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 const program = require("commander");
 const fs = require("fs");
+const marked = require("marked");
 
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
 
-// ファイルを非同期で読み込む
+// コマンドライン引数のオプションを取得する
+const options = program.opts();
+
+// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
+const cliOptions = {
+    gfm: options.gfm ?? false,
+};
+
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
     if (err) {
         console.error(err.message);
-        // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
         process.exit(1);
         return;
     }
-    console.log(file);
+    const html = marked.parse(file, {
+        // オプションの値を使用する
+        gfm: cliOptions.gfm,
+    });
+    console.log(html);
 });
